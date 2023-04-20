768e1e583abb99732216212fef5a24935ad90ba9
CrazySoldierboy <110178968+CrazySoldierboy@users.noreply.github.com>
2023-04-20 21:41:13 +0300

    Add files via upload

---
+++ Option1.java
@@ -0,0 +1,13 @@
+public class Option1 {
+    public static void main(String[] args) {
+        if (args[0].equals("-en")){
+            System.out.println("Опция для английского языка");
+        }
+        else if (args[0].equals("-es")){
+            System.out.println("Опция для Испанского языка");
+        }
+        else{
+            System.out.println("Неизвестная опция");
+        }
+    }
+}
