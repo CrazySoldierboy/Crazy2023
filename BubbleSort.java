04e4d2f14fef3ccdebd81f506aa441c83ff17c3a
CrazySoldierboy <110178968+CrazySoldierboy@users.noreply.github.com>
2023-06-19 13:38:45 +0300

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,35 +1,31 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        int [] box = new int[6];
-        box[0] = 5;
-        box[1] = 1;
-        box[2] = 3;
-        box[3] = 2;
-        box[5] = 4;
-
-        System.out.print("Массив в начале = ");
-        for(int i = 0; i <= box.length -1; i++){
+        // Массив
+        int[] box = {5, 1, 3, 2, 4};
+        System.out.print("Массив до сортировки = ");
+        for(int i = 0; i <= box.length - 1; i++){
             System.out.print(box[i]);
             System.out.print(", ");
         }
         System.out.println();
-
+        // Пустая переменная
         int temp;
-
-        for(int i = 0; i < box.length - 1; i++){
-           if(box[i] > box[i+1]){
-               temp = box[i+1];
-               box[i+1] = box[i];
-               box[i] = temp;
-           }else{
-               continue;
-           }
+        
+        for(int i = 0; i <= box.length - 1; i++){
+            for(int j = box.length - 1; j > i; j--){
+                if(box[j - 1] > box[j]){
+                    temp = box[j];
+                    box[j] = box[j - 1];
+                    box[j - 1] = temp;
+                }
+            }
         }
-
         System.out.print("Массив после сортировки = ");
-        for(int i = 0; i <= box.length -1; i++){
+        for(int i = 0; i <= box.length - 1; i++){
             System.out.print(box[i]);
             System.out.print(", ");
         }
+
+
     }
 }
